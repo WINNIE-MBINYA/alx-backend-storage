56008620d3f9274d966eb8e886857015e49824f7
WINNIE-MBINYA <winnymbinyamwanza@gmail.com>
2024-07-16 15:57:33 +0300

    Correct checker errors

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,4 +1,4 @@
 -- Removes all corrections with a score of 0
 
-DELETE FROM corrections
-WHERE score = 0;
+CREATE VIEW need_meeting AS SELECT name from students WHERE score < 80
+AND (last_meeting IS NULL OR last_meeting < DATE(CURDATE() - INTERVAL 1 MONTH));
