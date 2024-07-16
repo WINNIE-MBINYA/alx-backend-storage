01df3467e1193dc024b5c0cda2f38979537d6c85
WINNIE-MBINYA <winnymbinyamwanza@gmail.com>
2024-07-16 15:42:15 +0300

    Correct script

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,10 @@
 -- Lists Glam rock bands ranked by their longevity
 
-SELECT band_name, 2022 - formed AS lifespan
+SELECT band_name,
+       CASE
+           WHEN formed IS NULL OR split IS NULL THEN 0
+           ELSE TIMESTAMPDIFF(YEAR, formed, IFNULL(split, '2022-01-01')) AS lifespan
+       END AS lifespan
 FROM metal_bands
-WHERE style = 'Glam rock'
+WHERE main_style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
