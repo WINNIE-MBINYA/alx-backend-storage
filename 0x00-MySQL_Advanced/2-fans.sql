dd917aa4cb84e812b6402a7c2156bdd082046dde
WINNIE-MBINYA <winnymbinyamwanza@gmail.com>
2024-07-16 14:57:19 +0300

    Correct script

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,6 @@
 -- Ranks country origins of bands by number of fans
 
-SELECT origin, SUM(nb_fans) AS nb_fans
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
