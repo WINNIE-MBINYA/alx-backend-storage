7c3190a6b8bec0cf093f0325bbed8540cfdc3b5b
WINNIE-MBINYA <winnymbinyamwanza@gmail.com>
2024-07-21 20:34:01 +0300

    Correct script

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -4,43 +4,44 @@ Provides stats about Nginx logs stored in MongoDB
 """
 from pymongo import MongoClient
 
-def log_stats():
-    """
-    Provides stats about Nginx logs stored in MongoDB
-    """
-    # Connect to the MongoDB server running locally on port 27017
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    # Access the 'nginx' collection in the 'logs' database
+
+def nginx_stats_check():
+    """ provides some stats about Nginx logs stored in MongoDB:"""
+    client = MongoClient()
     collection = client.logs.nginx
 
-    # Count the total number of documents in the collection
-    num_logs = collection.count_documents({})
-    print(f"{num_logs} logs")
-
+    num_of_docs = collection.count_documents({})
+    print("{} logs".format(num_of_docs))
     print("Methods:")
-    # List of HTTP methods to check in the logs
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    # Count and print the number of documents for each HTTP method
-    for method in methods:
-        count = collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
-
-    # Count and print the number of documents with method "GET" and path "/status"
-    status_check = collection.count_documents({"method": "GET", "path": "/status"})
-    print(f"{status_check} status check")
-
-    # Aggregation pipeline to get the top 10 most present IPs
-    pipeline = [
-        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},  # Group by IP and count occurrences
-        {"$sort": {"count": -1}},  # Sort by count in descending order
-        {"$limit": 10}  # Limit results to top 10
-    ]
-    top_ips = list(collection.aggregate(pipeline))
+    methods_list = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods_list:
+        method_count = collection.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, method_count))
+    status = collection.count_documents({"method": "GET", "path": "/status"})
+    print("{} status check".format(status))
 
     print("IPs:")
-    # Print the top 10 most present IPs
-    for ip in top_ips:
-        print(f"\t{ip['_id']}: {ip['count']}")
+
+    top_IPs = collection.aggregate([
+        {"$group":
+         {
+             "_id": "$ip",
+             "count": {"$sum": 1}
+         }
+         },
+        {"$sort": {"count": -1}},
+        {"$limit": 10},
+        {"$project": {
+            "_id": 0,
+            "ip": "$_id",
+            "count": 1
+        }}
+    ])
+    for top_ip in top_IPs:
+        count = top_ip.get("count")
+        ip_address = top_ip.get("ip")
+        print("\t{}: {}".format(ip_address, count))
+
 
 if __name__ == "__main__":
-    log_stats()
+    nginx_stats_check()
